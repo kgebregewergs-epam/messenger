aaf93a6c04bc996d1a1e4cb52f9c9dce89f58d1f
kgebregewergs-epam <128231028+kgebregewergs-epam@users.noreply.github.com>
2023-03-18 22:18:59 -0500

    Added logical unit to which are  all red or the first stage of TDD for TemplateEngineTest.

--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,38 @@
 package com.epam.ld.module2.testing.template;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class TemplateEngineTest {
+
+    @Test
+    public void checkGenerateMessageWithValidTemplateFromConsole() {
+        fail();
+    }
+
+    @Test
+    public void checkGenerateMessageWithInValidTemplateFromConsole() {
+        fail();
+    }
+
+    @Test
+    public void shouldThrowExceptionIfNonOfThePlaceHoldersAreNotProvidedFromConsole() throws IllegalArgumentException {
+        fail();
+    }
+
+    @Test
+    public void checkGenerateMessageWithValidTemplateFromFile() {
+        fail();
+    }
+
+    @Test
+    public void checkGenerateMessageWithInValidTemplateFromFile() {
+        fail();
+    }
+
+    @Test
+    public void shouldThrowExceptionIfNonOfThePlaceHoldersAreNotProvidedFromFile() throws IllegalArgumentException {
+        fail();
+    }
 }
