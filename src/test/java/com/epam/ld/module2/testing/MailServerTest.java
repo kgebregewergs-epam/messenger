7c0c685f5753eddf9a85bcb0a09a8cadd600016c
kgebregewergs-epam <128231028+kgebregewergs-epam@users.noreply.github.com>
2023-03-20 07:30:06 -0500

    Added the initial unit test for the business logic of MailServerTest.

--- src/test/java/com/epam/ld/module2/testing/MailServerTest.java
+++ src/test/java/com/epam/ld/module2/testing/MailServerTest.java
@@ -1,4 +1,23 @@
 package com.epam.ld.module2.testing;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class MailServerTest {
+
+    @Test
+    public void checkSuccessfulSendMessage() {
+        fail();
+    }
+
+    @Test
+    public void checkUnSuccessfulSendingMessage() {
+        fail();
+    }
+
+    @Test
+    public void shouldThrowNullPointerExecution() {
+        fail();
+    }
 }
