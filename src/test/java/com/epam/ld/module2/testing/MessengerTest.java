dd66ecefa6828a94be31fc784103881e291f03aa
kgebregewergs-epam <128231028+kgebregewergs-epam@users.noreply.github.com>
2023-03-20 08:44:53 -0500

    Added the first step for messengerTest. in this step I added the business logic unit test with each implementation of failed or Red step of TDD.

---
+++ src/test/java/com/epam/ld/module2/testing/MessengerTest.java
@@ -0,0 +1,23 @@
+package com.epam.ld.module2.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MessengerTest {
+
+    @Test
+    public void checkSendMessageSuccessfully() {
+        fail();
+    }
+
+    @Test
+    public  void checkSendMessageShouldFailWhenClientIsNull() {
+        fail();
+    }
+
+    @Test
+    public  void checkSendMessageShouldFailWhenTemplateEngineIsNull() {
+        fail();
+    }
+}
