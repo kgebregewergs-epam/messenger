51c649f6e2f44f9fa280840aca1aebb914be4843
kgebregewergs-epam <128231028+kgebregewergs-epam@users.noreply.github.com>
2023-03-20 08:36:57 -0500

    added the second step to MailServerTest.

--- src/main/java/com/epam/ld/module2/testing/MailServer.java
+++ src/main/java/com/epam/ld/module2/testing/MailServer.java
@@ -4,13 +4,23 @@ package com.epam.ld.module2.testing;
  * Mail server class.
  */
 public class MailServer {
-
     /**
      * Send notification.
      *
      * @param addresses  the addresses
      * @param messageContent the message content
      */
-    public void send(String addresses, String messageContent) {
+    public Boolean send(String addresses, String messageContent) {
+        if (addresses == null && messageContent == null) {
+            throw new NullPointerException("The addresses and message content must be not null");
+        }
+
+        if (addresses != null && messageContent != null) {
+            System.out.format("Sending the message Successfully message:  ", messageContent);
+            return true;
+        }
+
+        System.out.format("Unable to Send the message Successfully");
+        return false;
     }
 }
--- src/test/java/com/epam/ld/module2/testing/MailServerTest.java
+++ src/test/java/com/epam/ld/module2/testing/MailServerTest.java
@@ -2,22 +2,32 @@ package com.epam.ld.module2.testing;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MailServerTest {
 
     @Test
     public void checkSuccessfulSendMessage() {
-        fail();
+        MailServer mailServer = new MailServer();
+        Boolean result  = mailServer.send("test@gmail.com", "just hi");
+        assertTrue(result);
     }
 
     @Test
     public void checkUnSuccessfulSendingMessage() {
-        fail();
+        MailServer mailServer = new MailServer();
+        Boolean result  = mailServer.send("test@gmail.com", null);
+        assertFalse(result);
     }
 
     @Test
-    public void shouldThrowNullPointerExecution() {
-        fail();
+    public void shouldThrowNullPointerExecution() throws NullPointerException {
+        MailServer mailServer = new MailServer();
+        try {
+            Boolean result  = mailServer.send(null, null);
+            fail();
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("Null pointer Exception: " + nullPointerException.getMessage());
+        }
     }
 }
