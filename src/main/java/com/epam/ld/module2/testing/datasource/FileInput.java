fd91af2263b72bd8027382095148ee0e200b8cdd
kgebregewergs-epam <128231028+kgebregewergs-epam@users.noreply.github.com>
2023-03-23 22:39:54 -0500

    Added a helper class that will hide the implementation from the main app.

--- src/main/java/com/epam/ld/module2/testing/datasource/FileInput.java
+++ src/main/java/com/epam/ld/module2/testing/datasource/FileInput.java
@@ -1,8 +1,60 @@
 package com.epam.ld.module2.testing.datasource;
 
+import org.apache.log4j.Logger;
+
+import java.io.*;
+
 /**
- * The interface to get input from file.
+ * The class to get input from file.
  */
-public interface FileInput {
-    Input getInput ();
+public class FileInput {
+    private static final Logger logger = Logger.getLogger(FileInput.class);
+    /**
+     * get Input.
+     * @param file
+     * @return an Input data
+     */
+    public Input getInput(String file) {
+        logger.info("The provided file path is: " + file);
+        String template = null, value = null, client = null;
+        BufferedReader bufferReader = null;
+        try {
+            File input = new File(file);
+            bufferReader  = new BufferedReader(new FileReader(input));
+            int inputSqs = 0;
+            String currentVal;
+            while ((currentVal = bufferReader.readLine()) != null) {
+                if(inputSqs == 0) {
+                    template = currentVal;
+                    inputSqs++;
+                }
+
+                else if(inputSqs == 1) {
+                    value = currentVal;
+                    inputSqs++;
+                }
+
+                else if (inputSqs == 2) {
+                    client = currentVal;
+                    inputSqs = 0;
+                }
+            }
+
+           return new Input(template, value, client);
+
+        } catch (IOException e) {
+            logger.error("Caught Exception: " + e.getMessage());
+
+        }
+
+        finally {
+            try {
+                bufferReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return new Input(null, null, null);
+    }
 }
